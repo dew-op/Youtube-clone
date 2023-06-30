01eb9eb4e179562fd3ace06676bf87099700ad6f
Dewansh Upadhyay <dewansh.upadhyay2017@gmail.com>
2023-07-01 00:15:01 +0530

    Update Classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,6 @@ import { FiBell } from "react-icons/fi";
 import { CgClose } from "react-icons/cg";
 
 import { Context } from "../context/contextApi";
-import Loader from "../shared/loader";
 
 const Header = () => {
     const [searchQuery, setSearchQuery] = useState("");
@@ -38,8 +37,6 @@ const Header = () => {
 
     return (
         <div className="sticky top-0 z-100 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
-
-            {/* {loading && <Loader />} */}
 
 
             {/* Main Youtube Logo */}
