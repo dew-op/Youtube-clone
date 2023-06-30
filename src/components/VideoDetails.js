bc842fd06365fea99c44e935664646532f43887c
Dewansh Upadhyay <dewansh.upadhyay2017@gmail.com>
2023-06-30 23:33:56 +0530

    Add Video Description

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -8,7 +8,8 @@ import { abbreviateNumber } from "js-abbreviation-number";
 import { fetchDataFromApi } from "../utils/api";
 import { Context } from "../context/contextApi";
 import SuggestionVideoCard from "./SuggestionVideoCard";
-import ShowMoreText from 'react-show-more-text';
+import ShowMoreText from "react-show-more-text";
+import { Interweave } from 'interweave';
 
 const VideoDetails = () => {
   const [video, setVideo] = useState();
@@ -44,7 +45,6 @@ const VideoDetails = () => {
     <div className="flex justify-center flex-row h-[calc(100%-56px)] bg-black">
       <div className="w-full max-w-[1280px] flex flex-col lg:flex-row">
         <div className="flex flex-col lg:w-[calc(100%-350px)] xl:w-[calc(100%-400px)] px-4 py-3 lg:py-6 overflow-y-auto">
-
           <div className="h-[200px] md:h-[400px] lg:h-[400px] xl:h-[550px] ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0">
             <ReactPlayer
               url={`https://www.youtube.com/watch?v=${id}`}
@@ -62,7 +62,6 @@ const VideoDetails = () => {
 
           <div className="flex justify-between flex-col md:flex-row mt-4">
             <div className="flex">
-
               <div className="flex items-start">
                 <div className="flex h-11 w-11 rounded-full overflow-hidden">
                   <img
@@ -84,11 +83,9 @@ const VideoDetails = () => {
                   {video?.author?.stats?.subscribersText}
                 </div>
               </div>
-
             </div>
 
             <div className="flex text-white mt-4 md:mt-0">
-
               <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15]">
                 <AiOutlineLike className="text-xl text-white mr-2" />
                 {`${abbreviateNumber(video?.stats?.views, 2)} Likes`}
@@ -97,16 +94,14 @@ const VideoDetails = () => {
               <div className="flex items-center justify-center h-11 px-6 rounded-3xl bg-white/[0.15] ml-4">
                 {`${abbreviateNumber(video?.stats?.views, 2)} Views`}
               </div>
-
             </div>
           </div>
 
           <div className="flex items-center justify-center h-auto ml-[-16px] lg:ml-0 mr-[-16px] lg:mr-0 bg-white/[0.15] text-white/[0.9] px-6 py-4 my-5 rounded-3xl">
-            <ShowMoreText>
-              {`${video?.description}`}
+            <ShowMoreText lines={4} width={1000000}>
+              <Interweave content={`${video?.description}`} />
             </ShowMoreText>
           </div>
-          
         </div>
 
         <div className="flex flex-col py-6 px-4 overflow-y-auto lg:w-[350px] xl:w-[400px]">
@@ -115,7 +110,6 @@ const VideoDetails = () => {
             return <SuggestionVideoCard key={index} video={item?.video} />;
           })}
         </div>
-
       </div>
     </div>
   );
